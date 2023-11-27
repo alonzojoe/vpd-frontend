e0a259fcc3a4dd3185f6d4ab2695c1a33eddcbb3
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-11-27 10:47:51 +0800

    Create actions and getters for Linelist module on store

--- src/store/modules/linelist.ts
+++ src/store/modules/linelist.ts
@@ -50,12 +50,49 @@ const actions = {
         const response = await api.get(`/linelist/?${queryParams}`)
         if (response.data.data.length > 0) {
             commit('setLnHeader', response.data.data)
+            commit('setTotalLn', response.data.total)
+            commit('setPaginatedLn', response.data.total_pages)
+        }
+    },
+
+    async saveLnHeader({ commit }, formData) {
+        const id = formData.id
+
+        if (id == 0) {
+            const response = await api.post('/linelist/header/create', {
+                linelist_code: formData.linelist_code,
+                dru: formData.dru,
+                dru_officer: formData.dru_officer,
+                contact: formData.contact,
+                region: formData.region,
+                province: formData.province,
+                email: formData.email,
+                created_by: formData.created_by,
+                updated_by: formData.updated_by,
+            });
+            commit('setLnResponse', response.data.data)
+        } else {
+            const response = await api.put(`/linelist/header/${id}`, {
+                linelist_code: formData.linelist_code,
+                dru: formData.dru,
+                dru_officer: formData.dru_officer,
+                contact: formData.contact,
+                region: formData.region,
+                province: formData.province,
+                email: formData.email,
+                created_by: formData.created_by,
+                updated_by: formData.updated_by,
+            })
+            commit('setLnResponse', response.data.data)
         }
     }
 }
 
 const getters = {
-
+    getLnHeader: state => state.data.lnheader,
+    getLnDetails: state => state.data.lndetails,
+    getLnTotal: state => state.data.totalLn,
+    getLnPaginated: state => state.data.paginatedLn
 }
 
 export default {
